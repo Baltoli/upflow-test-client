6e1c3033b71aaf135b3b5767782596a7f40c1f21
Bruce Collie <brucecollie82@gmail.com>
2021-03-04 21:42:25 +0000

    Echo on success / error callbacks

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Webhooks');
+import express from 'express';
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.post('/success', (req, res) => {
+  console.log('success: ' + JSON.stringify(req.body));
+  res.sendStatus(200);
+});
+
+app.post('/error', (req, res) => {
+  console.error('error: ' + JSON.stringify(req.body));
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Webhook client app listening on port: ${port}`);
+});
